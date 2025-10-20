d4d213771b3be4b231b30d481159fd24246108c0
emanherawy <eman.herawy@gmail.com>
2025-10-20 21:56:43 +0300

    add OZ Governor

--- backend/contracts/script/FlowGovernor.s.sol
+++ backend/contracts/script/FlowGovernor.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {FlowGovernor} from "../src/FlowGovernor.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract FlowGovernorScript is Script {
+    FlowGovernor public governor;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        // counter = new Counter();
 
         vm.stopBroadcast();
     }
---
+++ backend/contracts/test/FlowGovernor.t.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FlowGovernor} from "../src/FlowGovernor.sol";
+
+contract FlowGovernorTest is Test {
+    FlowGovernor public governor;
+
+    function setUp() public {
+      //  cogovernorunter = new FlowGovernor();
+     }
+
+
+
+    function testFuzz_SetNumber(uint256 x) public {
+     
+    }
+}
